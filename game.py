d5dcf4185160ef629d876f6e699494d28c3dea1c
Chris <christian.birch@gmail.com>
2016-08-22 17:31:11 +0100

    Added the numberOfPlayersInGame() and setPlayerTypes()

--- game.py
+++ game.py
@@ -2,9 +2,10 @@
 
 import subprocess as sp
 
-
+# Main variables
 numberOfPlayers = 0
-playerTypes = {}
+playerTypes = []
+cards = []
 
 # Function to clear the screen
 def clearScreen():	
@@ -13,18 +14,58 @@ def clearScreen():
 def numberOfPlayersInGame():	
 	AllOptionsChosen = False
 
-	while AllOptionsChosen == False:
-		global numberOfPlayers
+	global numberOfPlayers
+	numberOfPlayers = 0
 
-		while numberOfPlayers <=0 and numberOfPlayers <=5:
-			numberOfPlayers = int(raw_input("How many people are playing? "))
+	print("\n\nPlease enter a number between 2 and 5\n")
+
+	while numberOfPlayers <= 1 or numberOfPlayers > 5:
+		
+		numberOfPlayers = raw_input("How many people are playing? ")
+		
+		try:
+			# check if the input is a number, cast as int
+			numberOfPlayers = int(numberOfPlayers)
+		except ValueError:
+			numberOfPlayers = 0		
+
+def setPlayerTypes():
+
+	if numberOfPlayers != 0:
+
+		global playerTypes
+
+		# Add extra player so the user never see's 'player 0'
+		numberOfPlayers = numberOfPlayers + 1
+
+		# as above, add extra 1
+		i = 1
+		option = ''
+
+		while i < numberOfPlayers:
+
+			option = raw_input("Player " + str(i)  + " will be [H]uman / [C]omputer? ")
+
+			if option == 'h' or option == 'H':
+				playerTypes.append("human")
+				i = i + 1			
+
+			elif option == 'c' or option == 'C':
+				playerTypes.append("computer")
+				i = i + 1
+
+			else:
+				print("\n # Please choose either [H]uman / [C]omputer #\n")
+
+	else:
+		print("\n # Please choose how many players are playing first. #")
 			
-			#AllOptionsChosen = True
-		break
-
-	print numberOfPlayers
+def createCards():
 
 
+def goFish():
+	# create a new list of cards
+	createCards()
 
 
 
@@ -37,11 +78,11 @@ while viewMenu == True:
 
 	# Print menu option
 	print("\n --- Game Setup ---")
-	print("\n1. Number of players")
-	print("2. Game Mode")
-	print("3. More information")
+	print("\n1. Number of players"), "[" + str(numberOfPlayers) + "]" if numberOfPlayers != 0 else ""
+	print("2. Player types")
+	print("3. Start game!")
 	print("\n")
-	print("Type 'q' to quit")
+	print("Type 'q' to go back to main menu")
 	print("\n")
 
 	# Get a choice
@@ -54,9 +95,10 @@ while viewMenu == True:
 		# Respond to the users choice
 		if choice == "1":
 			numberOfPlayersInGame()
+			break
 		
 		elif choice == "2":
-			howToPlay()
+			setPlayerTypes()
 			break
 
 		elif choice == "3":
